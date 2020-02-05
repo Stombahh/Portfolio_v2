a179e45ae359db7c777216ca9ea046d5a6e5f1cb
Stombahh <tommi.ilvonen@gmail.com>
2020-02-05 22:40:19 +0200

    Made progress with Skills component

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,61 +1,108 @@
 import React from 'react'
-import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 import { DiJavascript1 } from 'react-icons/di';
 import { DiHtml5 } from 'react-icons/di';
 import { DiCss3 } from 'react-icons/di';
 import { FaVuejs } from 'react-icons/fa';
+import { FaReact } from 'react-icons/fa';
+import { FaJenkins } from 'react-icons/fa';
+import { FaDocker } from 'react-icons/fa';
+import { FaGitAlt } from 'react-icons/fa';
 
 import styles from './Skills.module.css';
 
 
 class Skills extends React.Component {
-  render() {
 
+  render() {
     return (
       <div className="wrapper">
-        <h1 style={{ marginBottom: '50px'}}>Skills</h1>
-        <div className={styles.skillsWrapper}>
-          <Card style={{ border: 'none', width: '18rem' }}>
-            <DiJavascript1/>
-            <Card.Body>
-              <Card.Title className={styles.cardTitle}>JavaScript</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card style={{ border: 'none', width: '18rem' }}>
-            <DiHtml5/>
-            <Card.Body>
-              <Card.Title className={styles.cardTitle}>HTML5</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card style={{ border: 'none', width: '18rem' }}>
-            <DiCss3/>
-            <Card.Body>
-              <Card.Title className={styles.cardTitle}>CSS3</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card style={{ border: 'none', width: '18rem' }}>
-            <FaVuejs/>
-            <Card.Body>
-              <Card.Title className={styles.cardTitle}>Vue</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
+        <Row>
+          <Col lg={4}>
+            <h2>Technologies</h2>
+            <Row>
+              <Col lg={4} xs={4}>
+                <div className={styles.skill}>
+                  <FaVuejs />
+                  <h4>Vue</h4>
+                </div>
+              </Col>
+              <Col lg={4} xs={4}>
+                <div className={styles.skill}>
+                  <FaReact />
+                  <h4>React</h4>
+                </div>
+              </Col>
+              <Col lg={4} xs={4}>
+                <div className={styles.skill}>
+                  <DiJavascript1 />
+                  <h4>JavaScript</h4>
+                </div>
+              </Col>
+              <Col lg={4} xs={4}>
+                <div className={styles.skill}>
+                  <DiHtml5 />
+                  <h4>HTML5</h4>
+                </div>
+              </Col>
+              <Col lg={4} xs={4}>
+                <div className={styles.skill}>
+                  <DiCss3 />
+                  <h4>CSS3</h4>
+                </div>
+              </Col>
+              <Col lg={4} xs={4}>
+                <div className={styles.skill}>
+                  <p>C#</p>
+                </div>
+              </Col>
+              <Col lg={7} xs={4}>
+                <div className={styles.skill}>
+                  <h2>Knockout.js</h2>
+                </div>
+              </Col>
+              <Col lg={4} xs={4}>
+                <div className={styles.skill}>
+                  <p>SQL</p>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={4}>
+            <h2>Tools</h2>
+            <Row>
+              <Col lg={4} xs={4}>
+                <div className={styles.skill}>
+                  <FaJenkins />
+                  <h4>Jenkins</h4>
+                </div>
+              </Col>
+              <Col lg={4} xs={4}>
+                <div className={styles.skill}>
+                  <FaDocker />
+                  <h4>Docker</h4>
+                </div>
+              </Col>
+              <Col lg={4} xs={4}>
+                <div className={styles.skill}>
+                  <FaGitAlt />
+                  <h4>Git</h4>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={4}><h2>Other</h2>
+            <Row>
+              <Col lg={6} xs={4}>
+                <div className={styles.skill}>
+                  <h2>Scrum</h2>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
       </div>
     )
   }
