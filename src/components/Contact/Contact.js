3a0b7056ece8b4c91388b5f80aa5749eadf928b0
Stombahh <tommi.ilvonen@gmail.com>
2020-03-04 21:58:01 +0200

    Trying to get Netlify form work

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,45 +4,18 @@ import Button from "react-bootstrap/Button";
 import styles from "./Contact.module.css";
 import Title from "../Title";
 
-// const Contact = () => {
-  // const [validated, setValidated] = React.useState(false);
+const Contact = () => {
+  const [validated, setValidated] = React.useState(false);
 
-  // const handleSubmit = event => {
-  //   const form = event.currentTarget;
-  //   if (form.checkValidity() === false) {
-  //     event.preventDefault();
-  //     event.stopPropagation();
-  //   }
-
-  //   setValidated(true);
-  // };
-
-  function encode(data) {
-    return Object.keys(data)
-      .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-      .join('&')
-  }
-  
-  export default function Contact() {
-    const [state, setState] = React.useState({})
-  
-    const handleChange = (e) => {
-      setState({ ...state, [e.target.name]: e.target.value })
+  const handleSubmit = event => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
     }
-  
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      const form = e.target
-      fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: encode({
-          'form-name': form.getAttribute('name'),
-          ...state,
-        }),
-      })
-        .catch((error) => alert(error))
-    }
+
+    setValidated(true);
+  };
 
   return (
     <div id="contact" className={styles.contactWrapper}>
@@ -50,46 +23,7 @@ import Title from "../Title";
       <p style={{ textAlign: "center" }}>
         Have a question or want to work together?
       </p>
-      <form
-        name="contact"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        onSubmit={handleSubmit}
-      >
-        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-        <input type="hidden" name="form-name" value="contact" />
-        <p hidden>
-          <label>
-            Don’t fill this out: <input name="bot-field" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your name:
-            <br />
-            <input type="text" name="name" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your email:
-            <br />
-            <input type="email" name="email" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message:
-            <br />
-            <textarea name="message" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-      {/* <Form
+      <Form
         name="contact"
         method="post"
         noValidate
@@ -98,7 +32,8 @@ import Title from "../Title";
         data-netlify-honeypot="bot-field"
         onSubmit={handleSubmit}
       >
-        <p className={styles.hidden}>
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
           <label>
             Don’t fill this out if you're human: <input name="bot-field" />
           </label>
@@ -148,9 +83,9 @@ import Title from "../Title";
           </Form.Control.Feedback>
         </Form.Group>
         <Button type="submit">Submit</Button>
-      </Form> */}
+      </Form>
     </div>
   );
 };
 
-// export default Contact;
+export default Contact;
